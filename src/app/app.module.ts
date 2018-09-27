e66d8f450e1fb30031fb35b49f9309883e31d577
Ricardo Vera <ricardo.vera4@gmail.com>
2018-09-27 16:30:48 -0300

    Prueba calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FullCalendarModule } from 'ng-fullcalendar';
 
 import { AppComponent } from './app.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
